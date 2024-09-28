e1f3e913edd14847cc44072e32b9de275451dc69
aziz-codes <azizisoffline@gmail.com>
2024-09-29 01:00:55 +0500

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ const Home = () => {
    
   const [code,setCode] = useState(`const name = "Hello,World"`);
   return (
-    <div className='flex flex-col gap-2'>
-      <div className='flex flex-col space-y-1'>
-        <label>Select Programming Language {lagnuage}</label>
-        <select onChange={(e)=>setLanguage(e.target.value.toLowerCase())}>
+    <div className='flex flex-col gap-2 '>
+      <div className='flex flex-col space-y-1 w-full max-w-xs px-2'>
+        <label>Select Programming Language</label>
+        <select className='py-1.5 rounded-md outline-none border' onChange={(e)=>setLanguage(e.target.value.toLowerCase())}>
         {languages.map((item)=>(
           <option key={item}>{item}</option>
         ))}
         </select>
       </div>
-      <Editor className=' h-[90vh]'  language={lagnuage} value={code} onChange={(e)=>setCode(e||"")}/>
+      <Editor className='h-[91vh]' language={lagnuage} value={code} onChange={(e)=>setCode(e||"")} theme='vs-dark'/>
     </div>
   )
 }
